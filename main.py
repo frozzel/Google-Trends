a4074cc1c6c29f6d57db47cd82df1a0828f0adbe
Dennis Hickox <frozzel@me.com>
2025-07-02 12:45:41 -0400

    some stuff

--- main.py
+++ main.py
@@ -16,14 +16,19 @@ print(f'Smallest value for Tesla in Web Search: {df_tesla.TSLA_WEB_SEARCH.min()}
 
 print(df_tesla.describe())
 
+# increases size and resolution
+plt.figure(figsize=(14,8), dpi=120) 
+plt.title('Tesla Web Search vs Price', fontsize=18)
+ 
 ax1 = plt.gca()
 ax2 = ax1.twinx()
  
-ax1.set_ylabel('TSLA Stock Price', color='#E6232E') # can use a HEX code
-ax2.set_ylabel('Search Trend', color='skyblue') # or a named colour
+# Also, increase fontsize and linewidth for larger charts
+ax1.set_ylabel('TSLA Stock Price', color='#E6232E', fontsize=14)
+ax2.set_ylabel('Search Trend', color='skyblue', fontsize=14)
  
-ax1.plot(df_tesla.MONTH, df_tesla.TSLA_USD_CLOSE, color='#E6232E')
-ax2.plot(df_tesla.MONTH, df_tesla.TSLA_WEB_SEARCH, color='skyblue')
-plt.title('Tesla Stock Price vs Search Trend')
-
+ax1.plot(df_tesla.MONTH, df_tesla.TSLA_USD_CLOSE, color='#E6232E', linewidth=3)
+ax2.plot(df_tesla.MONTH, df_tesla.TSLA_WEB_SEARCH, color='skyblue', linewidth=3)
+ 
+# Displays chart explicitly
 plt.show()
